7e51ade77d0d78e1589bca34cdc6dcf9f40f9299
Tim Goshinski <tim@code-chimp.com>
2015-07-04 14:54:11 -0500

    Base - shortening the build scripts names as I am a lazy bastid

--- bare/gulpfile.js/tasks/build-dev.js
+++ bare/gulpfile.js/tasks/build-dev.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
-gulp.task('build:development', function (cb) {
+gulp.task('build:dev', function (cb) {
   gulpSequence('clean', ['images'], ['sass'], ['watch', 'browserSync'], cb);
 });
--- bare/gulpfile.js/tasks/build-prod.js
+++ bare/gulpfile.js/tasks/build-prod.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
-gulp.task('build:development', function (cb) {
+gulp.task('build:prod', function (cb) {
   process.env.NODE_ENV = 'production';
   gulpSequence('clean', ['images'], ['sass'], cb);
 });
