216256f7f0336c0ec770f6998656f658d911f99b
Tim Goshinski <tim@code-chimp.com>
2015-07-19 00:01:26 -0500

    Bare - integrated gulp-data with nunjucks render

--- bare/gulpfile.js/config/html.js
+++ bare/gulpfile.js/config/html.js
@@ -4,6 +4,8 @@ module.exports = {
   watch: config.sourceDirectory + '/views/**/*.html',
   src: [config.sourceDirectory + '/views/**/*.html', '!**/{layouts,shared}/**'],
   dest: config.distDirectory,
+  dataDirectory: config.sourceDirectory + '/views/data/',
+  siteInfo: config.sourceDirectory + '/views/data/site-info.json',
   nunjucks: [config.sourceDirectory + '/views/'],
   htmlmin: {
     collapseWhitespace: true
--- bare/gulpfile.js/tasks/html.js
+++ bare/gulpfile.js/tasks/html.js
@@ -1,10 +1,35 @@
 var gulp = require('gulp');
+var data = require('gulp-data');
 var gulpIf = require('gulp-if');
 var render = require('gulp-nunjucks-render');
 var htmlmin = require('gulp-htmlmin');
+var fs = require('fs');
+var path = require('path');
+var assign = require('object-assign');
 var browserSync = require('browser-sync');
 var config = require('../config/html');
 var handleErrors = require('../lib/handle-errors');
+var siteInfo = JSON.parse(fs.readFileSync(config.siteInfo));
+
+// create a nicely formatted copyright
+var copyrightStart = siteInfo.copyrightStart;
+var currentYear = new Date().getFullYear();
+var copyrightLine = copyrightStart;
+
+if (currentYear > copyrightStart) {
+  copyrightLine = copyrightStart + '&mdash;' + currentYear;
+}
+
+siteInfo.copyrightLine = copyrightLine;
+
+// merge page specific data with the general site data
+var getPageData = function (file) {
+  var pageInfo = JSON.parse(
+    fs.readFileSync(config.dataDirectory + path.basename(file.path) + '.json'));
+  var pageData = assign(siteInfo, pageInfo);
+
+  return pageData;
+};
 
 gulp.task('html', function () {
   // disable nunjucks watch
@@ -13,6 +38,7 @@ gulp.task('html', function () {
   });
 
   return gulp.src(config.src)
+    .pipe(data(getPageData))
     .pipe(render())
     .on('error', handleErrors)
     .pipe(gulpIf(process.env.NODE_ENV === 'production', htmlmin(config.htmlmin)))
