6cea1774728ceea3d1549019730edcd9a9cb073f
Tim Goshinski <tim@code-chimp.com>
2015-07-23 20:50:33 -0500

    Bare
     - fixed nav menu for collapsed view
     - tweaked font sized and line heights in menu (way too large)
     - changed jscs to exclude esprima since there's no jsx in this proj
     - added text to logo
     - minor nudges here and there

--- bare/gulpfile.js/config/jscs.js
+++ bare/gulpfile.js/config/jscs.js
@@ -1,5 +1,5 @@
 var config = require('./');
 
 module.exports = {
-  src: [config.sourceAssets + '/js/**/*.{js,jsx}', 'gulpfile.js/**/*.js']
+  src: [config.sourceAssets + '/js/**/*.js', 'gulpfile.js/**/*.js']
 };
