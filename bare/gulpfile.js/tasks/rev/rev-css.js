7952665770aeda1795f44fa0b444695132c0107a
Tim Goshinski <tim.goshinski@idea5inc.com>
2015-07-27 07:18:46 -0500

    bare - don't know why I didn't push the rev-napkin up?

--- bare/gulpfile.js/tasks/rev/rev-css.js
+++ bare/gulpfile.js/tasks/rev/rev-css.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var filter = require('gulp-filter');
 var minify = require('gulp-minify-css');
 var rev = require('gulp-rev');
+var revNapkin = require('gulp-rev-napkin');
 var uglify = require('gulp-uglify');
 var config = require('../../config');
 
@@ -9,6 +10,7 @@ gulp.task('rev-css', ['rev-update-references'], function () {
   return gulp.src(config.distDirectory + '/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest(config.distDirectory))
+    .pipe(revNapkin({verbose: false}))
     .pipe(rev.manifest('dist/rev-manifest.json', {merge: true}))
     .pipe(gulp.dest(''));
 });
